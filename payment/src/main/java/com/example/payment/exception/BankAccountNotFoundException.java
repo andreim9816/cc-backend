3b2a68c6debfb1f6c9ebccf10c1af516512f4e61
Andrei Manolache <48209373+andreim9816@users.noreply.github.com>
2023-01-10 00:10:41 +0200

    CI/CD ; deleted other modules (#8)

--- payment/src/main/java/com/example/payment/exception/BankAccountNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.payment.exception;
-
-public class BankAccountNotFoundException extends RuntimeException {
-  public BankAccountNotFoundException(String message) {
-    super(message);
-  }
-}
---
+++ src/main/java/com/example/bankingapi/BankingApiApplication.java
@@ -0,0 +1,17 @@
+package com.example.bankingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EntityScan(basePackages = {"com.example.bankingapi.domain"})
+@EnableFeignClients
+public class BankingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingApiApplication.class, args);
+    }
+
+}
